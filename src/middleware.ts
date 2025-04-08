8c9a5a5d767da44d2f1b8c4a871e8587949230d7
Diicatus <dicatuswatch4@gmail.com>
2025-04-09 01:18:45 +0200

    Añadido middleware de autenticación

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const basicAuth = request.headers.get('authorization')
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, password] = atob(authValue).split(':')
+
+    // Cambia 'admin' y 'pokerelay' por los que tú quieras
+    if (user === 'admin' && password === 'pokerelay') {
+      return NextResponse.next()
+    }
+  }
+
+  return new NextResponse('Auth Required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
+
+export const config = {
+  matcher: '/:path*',
+}
